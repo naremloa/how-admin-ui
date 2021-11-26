2c2e836ec8628083facd5480bbbabaef032d8690
naremloa <naremloa@howdesign.con.tw>
2021-11-26 22:57:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <h1 class="text-center text-3xl">PLAYGROUND</h1>
+</template>
